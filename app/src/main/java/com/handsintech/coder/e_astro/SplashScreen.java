dbe3b3ea50e2042d6b12786103fd7294caf26240
Prathamesh <tamteprathamesh@gmail.com>
2018-06-14 11:55:24 +0530

    working on splash screen and other component

---
+++ app/src/main/java/com/handsintech/coder/e_astro/SplashScreen.java
@@ -0,0 +1,50 @@
+package com.handsintech.coder.e_astro;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+   // requestWindowFeature(Window.FEATURE_NO_TITLE);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        setContentView(R.layout.activity_splash_screen);
+//a1=getActionBar();
+        //       a1.hide();
+
+        // getSupportActionBar().hide();
+
+
+
+        // getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        // getActionBar().hide();
+        Thread myThread =new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(2000);
+                    Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+    }
+
